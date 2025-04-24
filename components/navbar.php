2fb5022ab064c77f300e626ad1a22c083d837fcd
eaterz <muraskoruslans@gmail.com>
2025-04-24 13:14:41 +0300

    teacher directories

--- components/navbar.php
+++ components/navbar.php
@@ -10,7 +10,11 @@
     } else if (Validator::Role('teacher')) {
       echo '
       <a href="/dashboard" class="btn btn-ghost mr-3">Dashboard</a>
+      <a href="/students" class="btn btn-ghost mr-3">Add Students</a>
+      <a href="/subjects" class="btn btn-ghost mr-3">Add Subjects</a>
+      <a href="/grades" class="btn btn-ghost mr-3">Add Grades</a>
       <a href="/logout" class="btn btn-ghost mr-3">Logout</a>';
+
       
     }
     ?>
